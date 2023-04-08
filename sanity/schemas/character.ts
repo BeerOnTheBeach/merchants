21516f284b0c8c0e4a4e5e258d475f2820befd1f
BeerOnTheBeach <gerproe@googlemail.com>
2023-04-08 12:50:22 +0200

    [TASK] Add character schema

---
+++ sanity/schemas/character.ts
@@ -0,0 +1,28 @@
+export default {
+    name: 'character',
+    type: 'document',
+    title: 'Character',
+    fields: [
+        {
+            name: 'name',
+            title: 'Name',
+            type: 'string'
+        },
+        {
+            name: 'money',
+            title: 'Geld',
+            type: 'number'
+        },
+        {
+            name: 'items',
+            title: 'Items',
+            type: 'array',
+            of: [{
+                name: 'item',
+                title: 'Item',
+                type: 'reference',
+                to: [{ type: 'item' }],
+            }],
+        }
+    ]
+}
